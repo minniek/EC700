b3d02191cc00e4bb0446dbc2d107cb8ac7214933
minniek <minniek@bu.edu>
2015-04-20 15:07:32 -0400

    updated delta3/views.py to include manual exceptions

--- Epoch3_Delta3/delta3/views.py
+++ Epoch3_Delta3/delta3/views.py
@@ -21,22 +21,39 @@ def comments(request):
 	return render(request, 'delta3/comments.html', {'form': CommentsForm})
 
 def search(request):
-	if(request.REQUEST.get('searchterm')):
-		searchterm = str(request.REQUEST.get('searchterm'))
-		sql = 'SELECT * from delta3_gif where gif_name=' + '"' + searchterm + '"' 
-		g = Gif.objects.raw(sql)
-		# g = Gif.objects.raw('SELECT * from delta3_gif where gif_name=""; SELECT * from delta3_user')
-		if (g):
-			response = ""
-			for x in g:	
-				response = response + str(x) + "<br>"
-			return HttpResponse(response)
+	if request.method == 'POST':
+		if(request.REQUEST.get('searchterm')):
+			searchterm = str(request.REQUEST.get('searchterm'))
+			sql = 'SELECT * from delta3_gif where gif_name=' + '"' + searchterm + '"' 
+			g = Gif.objects.raw(sql)
+			# g = Gif.objects.raw('SELECT * from delta3_gif where gif_name=""; SELECT * from delta3_user')
+			if (g):
+				response = ""
+				for x in g:	
+					response = response + str(x) + "<br>"
+					url = str(x.gif_url)
+				html = "<img src=" + url + "\>"
+				return HttpResponse(html)
 	return render(request, 'delta3/search.html', {'form': SearchForm})
 
 def about(request):
 	return render(request, 'delta3/about.html')
 
 def register(request):
+	# Exceptions are manually being created for POC
+	# These exceptions will handled by the middlewares.py's "process_exception"
+	# Raise ValueError exception if a digit is in firstname or lastname
+	if (request.REQUEST.get('firstname')):
+		firstname_in = request.REQUEST.get('firstname')
+		if any(char.isdigit() for char in firstname_in):
+			raise ValueError("Integer detected in firstname in /delta3/register")
+
+	if (request.REQUEST.get('lastname')):
+		lastname_in = request.REQUEST.get('lastname')
+		if any(char.isdigit() for char in lastname_in):
+			raise ValueError("Integer detected in lastname in /delta3/register")
+
+	# Raise ValueError exception if a character is found in the grad_year
 	if(request.REQUEST.get('grad_year')):
 		x = int(request.REQUEST.get('grad_year'))
 		# try: 
